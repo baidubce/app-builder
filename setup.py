78636bc61d9168fbb2b450d6cfcf7e320e090a44
tangwei12 <tangwei12@baidu.com>
2024-01-03 17:51:48 +0800

    fix version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='appbuilder-sdk',
-    version='0.1.0',
+    version='0.2.0',
     author='dongdaxiang',
     author_email='dongdaxiang@baidu.com',
     packages=find_packages(),
