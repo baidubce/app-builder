cc0371cb89e3be38aa56e432c1fce6f84f358ab8
tangwei12 <tangwei12@baidu.com>
2024-02-08 11:37:14 +0800

    新增0.3.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='appbuilder-sdk',
-    version='0.3.0',
+    version='0.3.1',
     author='dongdaxiang',
     author_email='dongdaxiang@baidu.com',
     packages=find_packages(),
