89a04933ee5b57ee7961702f81cb72b0be546391
hermitgreen <yangbo25@baidu.com>
2024-03-21 20:03:14 +0800

    物体识别 fc bad case fix

--- appbuilder/core/components/image_understand/component.py
+++ appbuilder/core/components/image_understand/component.py
@@ -198,7 +198,7 @@ class ImageUnderstand(Component):
             req.url = file_urls[img_path]
         if img_url:
             if img_url in file_urls:
-                img_url = file_urls['img_url']
+                img_url = file_urls[img_url]
             req.url = img_url
         response = self.__recognize(req)
         description_to_llm = response.result.description_to_llm
