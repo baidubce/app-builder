eb9d527e210fc2e00084504d4d7c82659c3ea804
C9luster <138663536+C9luster@users.noreply.github.com>
2024-04-30 00:06:20 -0700

    test_console_dataset (#291)
    Co-authored-by: C9luster <yinjiaqi@baidu.com>

--- appbuilder/tests/test_console_dataset.py
+++ appbuilder/tests/test_console_dataset.py
@@ -16,7 +16,7 @@ import os
 import unittest
 import appbuilder
 
-@unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_PARALLEL", "")
+@unittest.skip(reason="数据集问题")
 class TestDataset(unittest.TestCase):
 
     @classmethod
