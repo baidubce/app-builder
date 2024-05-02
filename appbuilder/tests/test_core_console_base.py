3a663e887bf2864319df7b44e1ebcfb63d1755b5
C9luster <yinjiaqi@baidu.com>
2024-05-02 03:55:32 -0700

    change_0502

--- appbuilder/tests/test_core_console_base.py
+++ appbuilder/tests/test_core_console_base.py
@@ -13,28 +13,117 @@
 # limitations under the License.
 import os
 import unittest
+import json
 
 from appbuilder.core.console.base import ConsoleLLMMessage,ConsoleCompletionResponse
 from appbuilder.core._exception import AppBuilderServerException
-from appbuilder.core.components.llms.base import CompletionResponse, LLMMessage
-from appbuilder.core.message import Message  
+from appbuilder.core.message import Message
 
+# 创建一个response类,模拟requests.Response
+class Response:
+    def __init__(self, status_code, headers, text):
+        self.status_code = status_code
+        self.headers = headers
+        self.text = text
+        
+    def json(self):
+            return json.loads(self.text)
+       
 
-# @unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_PARALLEL", "")
-@unittest.skip(reason="暂时跳过")
+@unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_PARALLEL", "")
 class TestCoreConsoleBase(unittest.TestCase):
     def test_ConsoleLLMMessage_init(self):
-        message=Message()
-        llmMessage=LLMMessage(message)
-        clm=ConsoleLLMMessage(llmMessage)
-        self.assertIsInstance(clm,ConsoleLLMMessage)
+        # 测试stream=True
+        cr=ConsoleLLMMessage()
+        self.assertIsInstance(str(cr), str)
         
     def test_ConsoleCompletionResponse_init(self):
-        # 测试stream=True
-        cr=CompletionResponse()
-        ccr=ConsoleCompletionResponse(response=cr,stream=True)
+        # 测试初始化,同时测试response.status_code != 200 and 
+        response=Response(
+            status_code=201,
+            headers={},
+            text=json.dumps({"message": "test"})
+        )
+        with self.assertRaises(AppBuilderServerException):
+            ConsoleCompletionResponse(response)
+            
+        # 测试if "code" in data and "message" in data and "requestId" in data
+        response=Response(
+            status_code=200,
+            headers={},
+            text=json.dumps({
+                "code": "test",
+                "requestId":"test",
+                "message":"test"
+                })
+        )
+        with self.assertRaises(AppBuilderServerException):
+            ConsoleCompletionResponse(response)
+            
+        # 测试if "code" in data and "message" in data and "status" in data
+        response=Response(
+            status_code=200,
+            headers={},
+            text=json.dumps({
+                "code": "test",
+                "message":"test",
+                "status":"test"
+                })
+        )
+        with self.assertRaises(AppBuilderServerException):
+            ConsoleCompletionResponse(response)
         
+        # if "code" in data and data.get("code") != 0
+        response=Response(
+            status_code=200,
+            headers={},
+            text=json.dumps({
+                "code": 1,
+                "message": "test"
+                })
+        )
+        with self.assertRaises(AppBuilderServerException):
+            ConsoleCompletionResponse(response)
+            
+        # 测试init其余部分
+        content = [
+            {
+                "content_type": "references",
+                "outputs": {
+                    "references": [
+                        {"from": "key"} 
+                        # 确认这是一个正确的字典
+                    ]
+                }
+            }
+        ]
+
+        # 序列化 content 列表为 JSON 字符串
+        content_json = json.dumps(content)
+
+        # 使用序列化后的 JSON 字符串作为响应文本
+        response = Response(
+            status_code=200,
+            headers={},
+            text=json.dumps({
+                "code": 0,
+                "result": {
+                    "answer": "test",
+                    "conversation_id": "test",
+                    "content": content  # 这里直接使用 content 变量
+                }
+            })
+        )
+        ccr=ConsoleCompletionResponse(response) 
         
+        # test_ConsoleCompletionResponse_message_iterable_wrapper
+        message = Message()
+        result = ccr.message_iterable_wrapper(message)
+  
+        #test_ConsoleCompletionResponse_to_message
+        message=ccr.to_message()
+        
+            
 
 if __name__ == '__main__':
     unittest.main()
