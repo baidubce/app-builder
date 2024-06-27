d2c9e6127aebeea380d1959d50dfa534f72d70ef
userpj <sudo_su@163.com>
2024-06-27 18:27:42 +0800

    add license

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2024 Baidu, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import time
 import uuid
 import os
@@ -106,7 +121,8 @@ class AppbuilderSDKInstance:
     def build_run_script(self):
         timestamp = int(time.time())
         self.run_script_name = "start_" + str(timestamp) + ".sh"
-        self.run_script = os.path.join(self.appbuilder_config["local_dir"], self.run_script_name)
+        self.run_script = os.path.join(
+            self.appbuilder_config["local_dir"], self.run_script_name)
 
         commands = []
         workspace = self.appbuilder_config["workspace"]
