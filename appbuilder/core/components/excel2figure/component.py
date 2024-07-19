e6e9404e8654ffbe5cc9130154e63ad49fbfca1a
ideoutrea <ideoutrea@163.com>
2024-07-19 16:21:31 +0800

    增加废弃Excel2Figure标记

--- appbuilder/core/components/excel2figure/component.py
+++ appbuilder/core/components/excel2figure/component.py
@@ -26,6 +26,7 @@ from appbuilder.core._exception import AppBuilderServerException, ModelNotSuppor
 from appbuilder.core.component import Component, ComponentArguments
 from appbuilder.core.message import Message
 from appbuilder.core.utils import ModelInfo, ttl_lru_cache
+from deprecated import deprecated
 from appbuilder.utils.trace.tracer_wrapper import components_run_trace, components_run_stream_trace
 
 
@@ -37,6 +38,7 @@ class Excel2FigureArgs(ComponentArguments):
     excel_file_url: AnyUrl = Field(..., description="用户的 excel 文件地址，需要是一个可被公网下载的 URL 地址")
 
 
+@deprecated(reason="组件后续不再更新, 可在AppBuilder官网创建智能体时配置代码解释器组件，可覆盖本组件的功能且效果更好", version="1.0")
 class Excel2Figure(Component):
     meta = Excel2FigureArgs
     model_type: str = "chat"
