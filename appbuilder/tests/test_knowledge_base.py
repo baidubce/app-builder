d2278b23cc56079fe30253fd3ca8e845ba3245e6
userpj <sudo_su@163.com>
2024-07-22 14:10:15 +0800

    python-sdk 新增知识库、知识库文档、切片增删改查等OpenAPI (#410)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * upadte
    
    * update
    
    * update
    
    * update
    
    * update unittest
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- appbuilder/tests/test_knowledge_base.py
+++ appbuilder/tests/test_knowledge_base.py
@@ -19,30 +19,119 @@ import os
 import time
 import logging
 
-@unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_SERIAL","")
+
+@unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_SERIAL", "")
 class TestKnowLedge(unittest.TestCase):
     def test_doc_knowledage(self):
         dataset_id = os.getenv("DATASET_ID", "UNKNOWN")
         knowledge = appbuilder.KnowledgeBase(knowledge_id=dataset_id)
 
         upload_res = knowledge.upload_file("./data/qa_appbuilder_client_demo.pdf")
-        add_res = knowledge.add_document(content_type='raw_text',
-                                         file_ids=[upload_res.id],
-                                         custom_process_rule=appbuilder.CustomProcessRule(
-                                            separators=["?"], target_length=400,overlap_rate=0.2
-                                         ))
+        add_res = knowledge.add_document(
+            content_type="raw_text",
+            file_ids=[upload_res.id],
+            custom_process_rule=appbuilder.CustomProcessRule(
+                separators=["?"], target_length=400, overlap_rate=0.2
+            ),
+        )
         list_res = knowledge.get_documents_list()
         delete_res = knowledge.delete_document(document_id=add_res.document_ids[0])
-    
+
     def test_xlsx_knowledage(self):
         dataset_id = os.getenv("DATASET_ID", "UNKNOWN")
         knowledge = appbuilder.KnowledgeBase(knowledge_id=dataset_id)
 
         upload_res = knowledge.upload_file("./data/qa_demo.xlsx")
-        add_res = knowledge.add_document(content_type='qa', file_ids=[upload_res.id])
+        add_res = knowledge.add_document(content_type="qa", file_ids=[upload_res.id])
         list_res = knowledge.get_documents_list()
         delete_res = knowledge.delete_document(document_id=add_res.document_ids[0])
 
+    def test_create_knowledge_base(self):
+        knowledge = appbuilder.KnowledgeBase()
+        resp = knowledge.create_knowledge_base(
+            name="test",
+            description="test",
+            type="public",
+            esUrl="http://localhost:9200",
+            esUserName="elastic",
+            esPassword="changeme",
+        )
 
-if __name__ == '__main__':
+        knowledge_base_id = resp.id
+        knowledge.get_knowledge_base_detail(knowledge_base_id)
+        knowledge.get_knowledge_base_list(knowledge_base_id, maxKeys=10)
+
+        err_msg = None
+        try:
+            knowledge.create_documents(
+                id=knowledge_base_id,
+                contentFormat="rawText",
+                source=appbuilder.DocumentSource(
+                    type="web",
+                    urls=["https://baijiahao.baidu.com/s?id=1802527379394162441"],
+                    urlDepth=1,
+                ),
+                processOption=appbuilder.DocumentProcessOption(
+                    template="custom",
+                    parser=appbuilder.DocumentChoices(
+                        choices=["layoutAnalysis", "ocr"]
+                    ),
+                    chunker=appbuilder.DocumentChunker(
+                        choices=["separator"],
+                        separator=appbuilder.DocumentSeparator(
+                            separators=["。"],
+                            targetLength=300,
+                            overlapRate=0.25,
+                        ),
+                        prependInfo=["title", "filename"],
+                    ),
+                    knowledgeAugmentation=appbuilder.DocumentChoices(choices=["faq"]),
+                ),
+            )
+
+            knowledge.upload_documents(
+                id=knowledge_base_id,
+                content_format="rawText",
+                file_path="./data/qa_appbuilder_client_demo.pdf",
+                processOption=appbuilder.DocumentProcessOption(
+                    template="custom",
+                    parser=appbuilder.DocumentChoices(
+                        choices=["layoutAnalysis", "ocr"]
+                    ),
+                    chunker=appbuilder.DocumentChunker(
+                        choices=["separator"],
+                        separator=appbuilder.DocumentSeparator(
+                            separators=["。"],
+                            targetLength=300,
+                            overlapRate=0.25,
+                        ),
+                        prependInfo=["title", "filename"],
+                    ),
+                    knowledgeAugmentation=appbuilder.DocumentChoices(choices=["faq"]),
+                ),
+            )
+
+            list_res = knowledge.get_documents_list()
+            document_id = list_res.data[-1].id
+            res = knowledge.describe_chunks(document_id)
+            resp = knowledge.create_chunk(document_id, content="test")
+            chunk_id = resp.id
+            knowledge.modify_chunk(chunk_id, content="new test", enable=True)
+            # 目前openapi有延迟，后续openapi完善后，删除注释
+            # knowledge.describe_chunk(chunk_id)
+            knowledge.delete_chunk(chunk_id)
+
+            knowledge.modify_knowledge_base(
+                knowledge_base_id=knowledge_base_id, name="test"
+            )
+        except Exception as e:
+            err_msg = str(e)
+            print("错误为 {}".format(e))
+        finally:
+            knowledge.delete_knowledge_base(knowledge_base_id)
+
+        self.assertIsNone(err_msg)
+
+
+if __name__ == "__main__":
     unittest.main()
