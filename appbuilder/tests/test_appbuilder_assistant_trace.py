a742c92e93aebc2ae4219cdb27fc69d247550a59
yinjiaqi <yinjiaqi@MacBook-Pro.local>
2024-07-24 09:25:48 +0800

    update

--- appbuilder/tests/test_appbuilder_assistant_trace.py
+++ appbuilder/tests/test_appbuilder_assistant_trace.py
@@ -74,7 +74,7 @@ class MyEventHandler(AssistantEventHandler):
                     ]
                 )
 
-@unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_SERIAL", "")
+# @unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_SERIAL", "")
 class TestAppBuilderTrace(unittest.TestCase):
     def setUp(self):
         """
@@ -129,10 +129,6 @@ class TestAppBuilderTrace(unittest.TestCase):
             assistant_id=assistant.id
         )
 
-        generator = appbuilder.assistant.threads.runs.stream_run(
-            thread_id=thread.id,
-            assistant_id=assistant.id
-        )
 
         tracer.end_trace()
 
@@ -140,57 +136,34 @@ class TestAppBuilderTrace(unittest.TestCase):
         tracer=AppBuilderTracer(
             enable_phoenix = True,
             enable_console = True,
-        )
+            )
+
+        tracer.start_trace()
+
         assistant = appbuilder.assistant.assistants.create(
             name="test_assistant",
             description="test assistant",
             instructions="每句话回复前都加上我是秦始皇"
         )
-        thread = appbuilder.assistant.threads.create(
-            [
-                {'content': 'hello world'}
-            ]
-        )
-        
-        run_result = appbuilder.assistant.threads.runs.stream_run(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
+
+        file = appbuilder.assistant.assistants.files.create(
+            "./data/qa_doc_parser_extract_table_from_doc.png"
         )
 
-        for run_step in run_result:
-            print(run_step)
-
-        tracer.end_trace()
-
-    def test_appbuilder_assistant_stream_event_handler(self):
-        tracer=AppBuilderTracer(
-            enable_phoenix = True,
-            enable_console = True,
-        )
-        assistant = appbuilder.assistant.assistants.create(
-            name="test_function",
-            description="你是一个热心的朋友",
-            instructions="请用友善的语气回答问题",
-            tools=[
-                {'type': 'function', 'function': check_tool}
-            ]
-        )
-        
         thread = appbuilder.assistant.threads.create()
-
         appbuilder.assistant.threads.messages.create(
             thread_id=thread.id,
-            content="今天北京的天气怎么样？",
+            content="hello world",
+            file_ids=[file.id]
+        )
+        generator = appbuilder.assistant.threads.runs.stream_run(
+            thread_id=thread.id,
+            assistant_id=assistant.id
         )
 
-        with appbuilder.assistant.threads.runs.stream_run_with_handler(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
-            event_handler=MyEventHandler(),
-        ) as stream:
-            for _ in stream:
-                ...
-
+        for run_step in generator:
+            print(run_step)
+        
         tracer.end_trace()
             
 
