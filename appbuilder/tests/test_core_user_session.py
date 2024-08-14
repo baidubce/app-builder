e7a81bf15dd67a66bba1a062eb2567a07dad2156
yinjiaqi <yinjiaqi@MacBook-Pro.local>
2024-08-14 16:05:25 +0800

    update

--- appbuilder/tests/test_core_user_session.py
+++ appbuilder/tests/test_core_user_session.py
@@ -22,6 +22,7 @@ from appbuilder.core.context import init_context,_LOCAL_KEY,get_context,context_
 from appbuilder.core.message import Message 
 
 
+
 @unittest.skipUnless(os.getenv("TEST_CASE", "UNKNOWN") == "CPU_PARALLEL", "")
 class TestCoreUserSession(unittest.TestCase):      
     def test_usersession_init(self):
