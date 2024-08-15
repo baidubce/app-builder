3d9ec03601c582f5f20418e96c89d7b017864fc0
userpj <sudo_su@163.com>
2024-08-15 22:11:44 +0800

    update

--- java/src/main/java/com/baidubce/appbuilder/console/appbuilderclient/AppBuilderClient.java
+++ java/src/main/java/com/baidubce/appbuilder/console/appbuilderclient/AppBuilderClient.java
@@ -39,7 +39,7 @@ public class AppBuilderClient extends Component {
      * 创建会话
      *
      * @return 返回会话ID
-     * @throws IOException               当请求失败时抛出IOException
+     * @throws IOException 当请求失败时抛出IOException
      * @throws AppBuilderServerException 当服务器返回错误码时抛出AppBuilderServerException
      */
     public String createConversation() throws IOException, AppBuilderServerException {
@@ -57,7 +57,8 @@ public class AppBuilderClient extends Component {
         ClassicHttpRequest postRequest = httpClient.createPostRequestV2(url,
                 new StringEntity(jsonBody, StandardCharsets.UTF_8));
         postRequest.setHeader("Content-Type", "application/json");
-        HttpResponse<ConversationResponse> response = httpClient.execute(postRequest, ConversationResponse.class);
+        HttpResponse<ConversationResponse> response =
+                httpClient.execute(postRequest, ConversationResponse.class);
         ConversationResponse respBody = response.getBody();
 
         return respBody.getConversationId();
@@ -67,9 +68,9 @@ public class AppBuilderClient extends Component {
      * 上传本地文件到指定会话中
      *
      * @param conversationId 会话ID
-     * @param filePath       文件路径
+     * @param filePath 文件路径
      * @return 上传后的文件ID
-     * @throws IOException               当请求失败时抛出IOException
+     * @throws IOException 当请求失败时抛出IOException
      * @throws AppBuilderServerException 当服务器返回错误码时抛出AppBuilderServerException
      */
     public String uploadLocalFile(String conversationId, String filePath)
@@ -84,15 +85,15 @@ public class AppBuilderClient extends Component {
             throw new RuntimeException("Param 'appID' is required!");
         }
         MultipartEntityBuilder builder = MultipartEntityBuilder.create()
-                .setMode(HttpMultipartMode.LEGACY)
-                .setCharset(StandardCharsets.UTF_8);
+                .setMode(HttpMultipartMode.LEGACY).setCharset(StandardCharsets.UTF_8);
         builder.addBinaryBody("file", new File(filePath));
         builder.addTextBody("app_id", this.appID);
         builder.addTextBody("conversation_id", conversationId);
         builder.addTextBody("scenario", "assistant");
 
         ClassicHttpRequest postRequest = httpClient.createPostRequestV2(url, builder.build());
-        HttpResponse<FileUploadResponse> response = httpClient.execute(postRequest, FileUploadResponse.class);
+        HttpResponse<FileUploadResponse> response =
+                httpClient.execute(postRequest, FileUploadResponse.class);
 
         FileUploadResponse respBody = response.getBody();
         return respBody.getFileId();
@@ -101,16 +102,16 @@ public class AppBuilderClient extends Component {
     /**
      * 运行AgentBuilder，根据输入的问题、会话ID、文件ID数组以及是否以流模式返回结果，返回AgentBuilderIterator迭代器。
      *
-     * @param query          查询字符串
+     * @param query 查询字符串
      * @param conversationId 会话ID
-     * @param fileIds        文件ID数组
-     * @param stream         是否以流的形式返回结果
+     * @param fileIds 文件ID数组
+     * @param stream 是否以流的形式返回结果
      * @return AgentBuilderIterator 迭代器，包含 AgentBuilder 的运行结果
-     * @throws IOException               如果在 I/O 操作过程中发生错误
+     * @throws IOException 如果在 I/O 操作过程中发生错误
      * @throws AppBuilderServerException 如果 AppBuilder 服务器返回错误
      */
-        public AppBuilderClientIterator run(String query, String conversationId, String[] fileIds, boolean stream)
-            throws IOException, AppBuilderServerException {
+    public AppBuilderClientIterator run(String query, String conversationId, String[] fileIds,
+            boolean stream) throws IOException, AppBuilderServerException {
         String url = AppBuilderConfig.AGENTBUILDER_RUN_URL;
         if (this.appID == null || this.appID.isEmpty()) {
             throw new RuntimeException("Param 'appID' is required!");
@@ -125,8 +126,24 @@ public class AppBuilderClient extends Component {
         ClassicHttpRequest postRequest = httpClient.createPostRequestV2(url,
                 new StringEntity(jsonBody, StandardCharsets.UTF_8));
         postRequest.setHeader("Content-Type", "application/json");
-        HttpResponse<Iterator<AppBuilderClientResponse>> response = httpClient.executeSSE(postRequest,
-                AppBuilderClientResponse.class);
+        HttpResponse<Iterator<AppBuilderClientResponse>> response =
+                httpClient.executeSSE(postRequest, AppBuilderClientResponse.class);
+        return new AppBuilderClientIterator(response.getBody());
+    }
+
+    public AppBuilderClientIterator run(AppBuilderClientRunRequest requestBody)
+            throws IOException, AppBuilderServerException {
+        String url = AppBuilderConfig.AGENTBUILDER_RUN_URL;
+        if (this.appID == null || this.appID.isEmpty()) {
+            throw new RuntimeException("Param 'appID' is required!");
+        }
+
+        String jsonBody = JsonUtils.serialize(requestBody);
+        ClassicHttpRequest postRequest = httpClient.createPostRequestV2(url,
+                new StringEntity(jsonBody, StandardCharsets.UTF_8));
+        postRequest.setHeader("Content-Type", "application/json");
+        HttpResponse<Iterator<AppBuilderClientResponse>> response =
+                httpClient.executeSSE(postRequest, AppBuilderClientResponse.class);
         return new AppBuilderClientIterator(response.getBody());
     }
 }
