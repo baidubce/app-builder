5a72cc937f4214653dc1c40afd3fd8f52d90c2e0
MrChengmo <cmchengmo@163.com>
2024-08-16 19:49:48 +0800

    update doc

--- appbuilder/core/components/text_to_image/model.py
+++ appbuilder/core/components/text_to_image/model.py
@@ -187,7 +187,7 @@ class Text2ImageQueryData(proto.Message):
             task_status(str):
                 计算总状态。有 INIT（初始化），WAIT（排队中）, RUNNING（生成中）, FAILED（失败）, SUCCESS（成功）四种状态，只有 SUCCESS 为成功状态。
             task_progress(float):
-                图片生成总进度，进度包含2种，0为未处理完，1为处理完成。
+                图片生成总进度，0到1之间的浮点数表示进度，0为未处理完，1为处理完成。
             sub_task_result_list(Text2ImageSubTaskResultList):
                 子任务生成结果列表。
          """
@@ -217,7 +217,7 @@ class Text2ImageSubTaskResultList(proto.Message):
             sub_task_status(int):
                 单风格图片状态。有 INIT（初始化），WAIT（排队中）, RUNNING（生成中）, FAILED（失败）, SUCCESS（成功）四种状态，只有 SUCCESS 为成功状态。
             sub_task_progress(float):
-                单任务图片生成进度，进度包含2种，0为未处理完，1为处理完成。
+                单任务图片生成进度，0到1之间的浮点数表示进度，0为未处理完，1为处理完成。
             sub_task_error_code(str):
                 单风格任务错误码。0:正常；501:文本黄反拦截；201:模型生图失败。
             final_image_list(Text2ImageFinalImageList):
