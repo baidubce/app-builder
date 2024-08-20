867639f8aa0126317e06fc0edd480ff9301fdb38
yinjiaqi <yinjiaqi@MacBook-Pro.local>
2024-08-20 16:51:23 +0800

    update

--- appbuilder/core/components/text_to_image/model.py
+++ appbuilder/core/components/text_to_image/model.py
@@ -17,146 +17,64 @@ r"""Text2Image model.py.
 """
 from typing import MutableSequence, List
 
+from typing import Optional,Union
 import proto
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 
-class Text2ImageSubmitRequest(proto.Message):
-    r"""文生图提交任务的请求体。
+class Text2ImageSubmitRequest(BaseModel):
+    prompt: str
+    width: int
+    height: int 
+    image_num: int = Field(default=1, ge=1, le=8)
+    image: Optional[str]
+    url: Optional[str] 
+    pdf_file: Optional[str] 
+    pdf_file_num: Optional[str]
+    change_degree: Optional[int] 
+    text_content: Optional[str] 
+    task_time_out: Optional[int]
+    text_check: Optional[int]
 
-         参数:
-            prompt(str):
-                生图的文本描述。仅支持中文、日常标点符号。不支持英文，特殊符号，限制 200 字。
-            width(int):
-                图片宽度，支持：512x512、640x360、360x640、1024x1024、1280x720、720x1280、2048x2048、2560x1440、1440x2560。
-            height(int):
-                图片高度，支持：512x512、640x360、360x640、1024x1024、1280x720、720x1280、2048x2048、2560x1440、1440x2560.
-            image_num(int):
-                生成图片数量，默认一张，支持生成 1-8 张。
-            image(string):
-                参考图，需 base64 编码，大小不超过 10M，最短边至少 15px，最长边最大 8192px，支持jpg/jpeg/png/bmp 格式。
-                优先级：image > url > pdf_file，当image 字段存在时，url、pdf_file 字段失效。
-            url(str):
-                参考图完整 url，url 长度不超过 1024 字节，url 对应的图片需 base64 编码，大小不超过 10M，最短边至少 15px，
-                最长边最大8192px，支持 jpg/jpeg/png/bmp 格式。优先级：image > url > pdf_file，当image 字段存在时，url 字段失效请注意关闭 URL 防盗链。
-            pdf_file(string):
-                参考图 PDF 文件，base64 编码，大小不超过10M，最短边至少 15px，最长边最大 8192px 。
-                优先级：image > url > pdf_file，当image 字段存在时，url、pdf_file 字段失效。
-            pdf_file_num(str):
-                需要识别的 PDF 文件的对应页码，当pdf_file 参数有效时，识别传入页码的对应页面内容，若不传入，则默认识别第 1 页。
-            change_degree(int):
-               参考图影响因子，支持 1-10 内；数值越大参考图影响越大。
-         """
-    prompt: str = proto.Field(
-        proto.STRING,
-        number=1,
-    )
-    width: int = proto.Field(
-        proto.INT32,
-        number=2,
-    )
-    height: int = proto.Field(
-        proto.INT32,
-        number=3,
-    )
-    image_num: int = proto.Field(
-        proto.INT32,
-        number=4,
-        optional=True,
-    )
-    image: str = proto.Field(
-        proto.STRING,
-        number=5,
-        optional=True,
-    )
-    url: str = proto.Field(
-        proto.STRING,
-        number=6,
-        optional=True,
-    )
-    pdf_file: str = proto.Field(
-        proto.STRING,
-        number=7,
-        optional=True,
-    )
-    pdf_file_num: str = proto.Field(
-        proto.STRING,
-        number=8,
-        optional=True,
-    )
-    change_degree: int = proto.Field(
-        proto.INT32,
-        number=9,
-        optional=True,
-    )
+class Text2ImageSubmitErrorDetail(BaseModel):
+    msg:Optional[str]
+    word:Optional[object]
 
 
-class Text2ImageQueryRequest(proto.Message):
-    r"""文生图生成结果查询请求体。
-
-         参数:
-            task_id(int):
-                从提交请求的提交接口的返回值中获取，可使用task_id 查询总任务。
-         """
-    task_id: int = proto.Field(
-        proto.INT64,
-        number=1,
-    )
+class Text2ImageSubmitResponse(BaseModel):
+    log_id:Optional[str]
+    data:Optional[object]
+    primary_task_id:Optional[int]
+    task_id:Optional[str]
+    error_msg:Optional[str]
+    error_detail:Optional[Text2ImageSubmitErrorDetail]
+    error_code: Optional[int]
 
 
-class Text2ImageSubmitResponse(proto.Message):
-    r"""文生图任务提交接口返回体。
-
-         参数:
-            request_id(str):
-                网关层的请求ID。
-            log_id(str):
-                算子层请求唯一标识码。
-            data(Text2ImageSubmitData):
-                任务提交接口返回数据。
-         """
-    request_id: str = proto.Field(
-        proto.STRING,
-        number=1,
-    )
-
-    log_id: int = proto.Field(
-        proto.INT64,
-        number=2,
-    )
-    data: "Text2ImageSubmitData" = proto.Field(
-        proto.MESSAGE,
-        number=3,
-        message="Text2ImageSubmitData",
-    )
+class Text2ImageQueryRequest(BaseModel):
+    task_id: Optional[str]
 
 
-class Text2ImageQueryResponse(proto.Message):
-    r"""文生图任务结果查询接口返回体。.
+class Text2ImageQueryResponseDate(BaseModel):
+    task_id: Optional[str]
+    task_status: Optional[str]
+    task_progress_detail: Optional[float]
+    task_progress: Optional[int]
+    sub_task_result_list: Optional[list[object]]
+    sub_task_status: Optional[str]
+    sub_task_progress_detail: Optional[float]
+    sub_task_progress: Optional[int]
+    sub_task_error_code: Optional[str]
+    final_image_list: Optional[list[object]]
+    img_url: Optional[str]
+    height: Optional[int]
+    width: Optional[int]
+    img_approve_conclusion: Optional[str]
 
-         参数:
-            request_id(str):
-                Request ID of gateway layer.
-            log_id(str):
-                Request ID of service layer.
-            data(Text2ImageQueryData):
-                Text to Image query response data .
-         """
-    request_id: str = proto.Field(
-        proto.STRING,
-        number=1,
-    )
 
-    log_id: int = proto.Field(
-        proto.INT64,
-        number=2,
-    )
-
-    data: "Text2ImageQueryData" = proto.Field(
-        proto.MESSAGE,
-        number=3,
-        message="Text2ImageQueryData",
-    )
+class Text2ImageQueryResponse(BaseModel):
+    log_id: Union[str,int,None] = None
+    data:Optional[Text2ImageQueryResponseDate]
 
 
 class Text2ImageSubmitData(proto.Message):
