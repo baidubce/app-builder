8754fb3bf101f2c593b0e8d569e3ccf03321b95f
代军 <jdaiuu@163.com>
2024-09-12 16:04:33 +0800

    修改了一点go文件，看看会不会触发

--- go/appbuilder/dataset_test.go
+++ go/appbuilder/dataset_test.go
@@ -30,7 +30,6 @@ func TestDataset(t *testing.T) {
 		t.Fatalf("create dataset failed: %v", err)
 	}
 	
-
 	documentID, err := dataset.UploadLocalFile(datasetID, "./files/test.pdf")
 	if err != nil {
 		t.Fatalf("upload file failed: %v", err)
