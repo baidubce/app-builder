595fe0094c6ff6474369bedf9fc96138b4ed236b
代军 <jdaiuu@163.com>
2024-09-18 22:07:45 +0800

    删除掉了故意添加的错误

--- go/appbuilder/agent_builder_test.go
+++ go/appbuilder/agent_builder_test.go
@@ -35,7 +35,7 @@ func TestNewAgentBuilder(t *testing.T) {
 	}
 
 	// 测试逻辑
-	config, err := NewSDKConfig("", "1")
+	config, err := NewSDKConfig("", "")
 	if err != nil {
 		t.Fatalf("new http client config failed: %v", err)
 	}
