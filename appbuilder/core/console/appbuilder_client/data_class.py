471f5965df09a038aeb37add8214b0dbdf678030
userpj <sudo_su@163.com>
2024-11-01 13:44:48 +0800

    update

--- appbuilder/core/console/appbuilder_client/data_class.py
+++ appbuilder/core/console/appbuilder_client/data_class.py
@@ -305,8 +305,8 @@ class AppOverview(BaseModel):
     id: str = Field("", description="应用ID")
     name: str = Field("", description="应用名称")
     description: str = Field("", description="应用简介")
-    appType: str = Field("", description="应用类型")
-    isPublished: bool = Field(False, description="是否已发布")
+    appType: str = Field(None, description="应用类型")
+    isPublished: bool = Field(None, description="是否已发布")
     updateTime: int = Field(None, description="更新时间")
 
 class AppBuilderClientAppListResponse(BaseModel):
