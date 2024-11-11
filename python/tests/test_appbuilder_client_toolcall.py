6ea6a52e8a5c6f378fc17e9cca9d72f84fcf5057
Mr.DJ <daijun04@baidu.com>
2024-11-11 17:52:17 +0800

    补充了相关单测

--- python/tests/test_appbuilder_client_toolcall.py
+++ python/tests/test_appbuilder_client_toolcall.py
@@ -31,8 +31,8 @@ class TestAgentRuntime(unittest.TestCase):
             None: 如果app_id不为空，则不会引发任何异常
             unittest.SkipTest (optional): 如果app_id为空，则跳过单测执行
         """
-        builder = appbuilder.AppBuilderClient(self.app_id)
-        conversation_id = builder.create_conversation()
+        client = appbuilder.AppBuilderClient(self.app_id)
+        conversation_id = client.create_conversation()
         tools = [
             {
                 "type": "function",
@@ -54,7 +54,7 @@ class TestAgentRuntime(unittest.TestCase):
             }
         ]
 
-        msg = builder.run(
+        msg = client.run(
             conversation_id=conversation_id,
             query="今天北京天气怎么样？",
             tools=tools)
@@ -64,7 +64,7 @@ class TestAgentRuntime(unittest.TestCase):
         assert event.status == "interrupt"
         assert event.event_type == "Interrupt"
 
-        msg_2 = builder.run(
+        msg_2 = client.run(
             conversation_id=conversation_id,
             tool_outputs=[
                 {
@@ -76,6 +76,51 @@ class TestAgentRuntime(unittest.TestCase):
         print(msg_2.model_dump_json(indent=4))
 
         
+        """测试functions2tools功能"""
+        #定义本地函数
+        def get_current_weather(location: str, unit: str) -> str:
+            """获取指定中国城市的当前天气信息。
+
+            仅支持中国城市的天气查询。参数 `location` 为中国城市名称，其他国家城市不支持天气查询。
+
+            Args:
+                location (str): 城市名，例如："北京"。
+                unit (int): 温度单位，支持 "celsius" 或 "fahrenheit"。
+
+            Returns:
+                str: 天气情况描述
+            """
+            return "北京今天25度"
+        #定义函数列表
+        functions = [get_current_weather]
+        function_map = {f.__name__: f for f in functions}
+        #调用大模型
+        msg = client.run(
+        conversation_id=conversation_id,
+        query="今天北京的天气怎么样？",
+        tools = [appbuilder.function_to_json(f) for f in functions]
+        )
+        print(msg.model_dump_json(indent=4))
+        # 获取最后的事件和工具调用信息
+        event = msg.content.events[-1]
+        tool_call = event.tool_calls[-1]
+
+        # 获取函数名称和参数
+        name = tool_call.function.name
+        args = tool_call.function.arguments
+
+        # 将函数名称映射到具体的函数并执行
+        raw_result = function_map[name](**args)
+
+        # 传递工具的输出
+        msg_2 = client.run(
+            conversation_id=conversation_id,
+            tool_outputs=[{
+                "tool_call_id": tool_call.id,
+                "output": str(raw_result)
+            }],
+        )
+        print(msg_2.model_dump_json(indent=4))   
         
 
 if __name__ == '__main__':
