66b60698b4373353418f73bbf7da667c751e5b34
Mr.DJ <daijun04@baidu.com>
2024-11-13 11:19:26 +0800

    去除无关代码

--- python/utils/tool_definition_decorator.py
+++ python/utils/tool_definition_decorator.py
@@ -560,31 +560,3 @@ def _update_list(new_item, list, condition, replacer):
         new_list.append(new_item)
     return new_list
 
-if __name__ == "__main__":
-    @function(description="anotated function", disable_docstring=True)
-    @function_parameter(name="param", description="a parameter", type="str", default_value="default_val")
-    @function_return(description="a result", default_value="default_result")
-    def func(param: str) -> str:
-        return param
-
-    view = func.__pf_function__
-
-    assert isinstance(view, FunctionView)
-    assert view.name == "func"
-    assert view.description == "anotated function"
-    assert view.is_async is False
-    assert view.is_stream is False
-
-    assert view.parameters[0].name == "param"
-    assert view.parameters[0].description == "a parameter"
-    assert view.parameters[0].default_value == "default_val"
-    assert view.parameters[0].type_ == "str"
-    assert view.parameters[0].required is True
-    assert view.parameters[0].example is None
-
-    assert view.returns[0].name == "return"
-    assert view.returns[0].description == "a result"
-    assert view.returns[0].default_value == "default_result"
-    assert view.returns[0].type_ == "str"
-    assert view.returns[0].required is True
-    assert view.returns[0].example is None
