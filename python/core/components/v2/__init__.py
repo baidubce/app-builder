ca8f1ad814805a5bde351b8a746610ad740bfebc
C9luster <138663536+C9luster@users.noreply.github.com>
2024-12-04 13:28:50 +0800

    Update components (#637)
    * SDK 组件V2版本更新[12-6]
    
    * 增加__init__函数
    
    * GeneralOCR组件接口更改,以及V2版本更新
    
    * GeneralOCR组件接口更改,以及V2版本更新[单测用例更新]
    
    * 补充单测
    
    * 更新注释
    
    ---------
    
    Co-authored-by: yinjiaqi <yinjiaqi@MacBook-Pro.local>

--- python/core/components/v2/__init__.py
+++ python/core/components/v2/__init__.py
@@ -14,6 +14,8 @@
 
 from .animal_recognize.component import AnimalRecognition
 from .image_understand.component import  ImageUnderstand
+from .translate.component import Translation
+from .general_ocr.component import GeneralOCR
 from .llms.style_rewrite.component import StyleRewrite
 from .llms.hallucination_detection.component import HallucinationDetection
 from .qrcode_ocr.component import QRcodeOCR
@@ -23,6 +25,8 @@ from .mix_card_ocr.component import MixCardOCR
 __V2_COMPONENTS__ = [
     "AnimalRecognition",
     "ImageUnderstand",
+    "Translation",
+    "GeneralOCR",
     "StyleRewrite",
     "HallucinationDetection",
     "QRcodeOCR",
