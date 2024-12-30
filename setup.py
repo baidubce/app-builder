7a08b9ea5cc80c7a7a53fe324ce2c2235dee1f74
yepeiwen01 <yepeiwen01@bidu.com>
2024-12-30 16:10:59 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ import shutil
 import datetime
 from setuptools import setup, find_packages
 
-from __init__ import __version__
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -56,7 +55,7 @@ all_require = serve_require + trace_require + test_require + langchain_require
 setup(
     name="appbuilder-sdk",
     # NOTE(chengmo): 修改此版本号时，请注意同时修改 __init__.py 中的 __version__
-    version=__version__,
+    version='0.9.8.3',
     author="dongdaxiang",
     author_email="dongdaxiang@baidu.com",
     packages=packages,
