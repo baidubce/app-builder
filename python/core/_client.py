70578174ed0773a859e0f5617e4472ac52c5147b
yinjiaqi <yinjiaqi@MacBook-Pro.local>
2024-12-30 10:26:10 +0800

    update

--- python/core/_client.py
+++ python/core/_client.py
@@ -259,30 +259,21 @@ class AsyncHTTPClient(HTTPClient):
         """
         status_code = response.status
         if status_code == requests.codes.ok:
-            logger.debug("request_id={} , http status code is {} , response text is {}".format(
-                await __class__.response_request_id(response), status_code, await response.text()
-            ))
             return
         message = "request_id={} , http status code is {}, body is {}".format(
             await __class__.response_request_id(response), status_code, await response.text()
         )
         if status_code == requests.codes.bad_request:
-            logger.error(message)
             raise BadRequestException(message)
         elif status_code == requests.codes.forbidden:
-            logger.error(message)
             raise ForbiddenException(message)
         elif status_code == requests.codes.not_found:
-            logger.error(message)
             raise NotFoundException(message)
         elif status_code == requests.codes.precondition_required:
-            logger.error(message)
             raise PreconditionFailedException(message)
         elif status_code == requests.codes.internal_server_error:
-            logger.error(message)
             raise InternalServerErrorException(message)
         else:
-            logger.error(message)
             raise BaseRPCException(message)
 
     @staticmethod
