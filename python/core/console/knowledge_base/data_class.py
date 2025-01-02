c2e7a3858ea31e0fbae51e53958b3954417d4e30
userpj <sudo_su@163.com>
2025-01-02 11:40:38 +0800

    知识库导入文档separator、prependInfo改为非必填 (#689)

--- python/core/console/knowledge_base/data_class.py
+++ python/core/console/knowledge_base/data_class.py
@@ -183,10 +183,10 @@ class DocumentChunker(BaseModel):
         description="使用哪些chunker方法 (separator | pattern | onePage)，separator：自定义切片—标识符，pattern：自定义切片—标识符中选择正则表达式，onePage：整文件切片",
     )
     prependInfo: list[str] = Field(
-        ...,
+        None,
         description="chunker关联元数据，可选值为title (增加标题), filename(增加文件名)",
     )
-    separator: Optional[DocumentSeparator] = Field(..., description="分段符号")
+    separator: Optional[DocumentSeparator] = Field(None, description="分段符号")
     pattern: Optional[DocumentPattern] = Field(None, description="正则表达式")
 
 
