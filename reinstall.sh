5792baf95d4a6a935b7f057fc82b82cd767d64e3
yepeiwen01 <yepeiwen01@bidu.com>
2025-03-12 16:42:18 +0800

    删除多余的修改

--- reinstall.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-pip3 uninstall -y appbuilder-sdk
-python3 setup.py bdist_wheel
-pip3 install dist/appbuilder_sdk-1.0.3-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ import shutil
 import datetime
 from setuptools import setup, find_packages
 
-
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
