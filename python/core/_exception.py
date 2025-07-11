fd1b731f10a3d7aea92128a66d1eeee20a9c582a
peiwenYe <963623403@qq.com>
2025-07-11 16:38:22 +0800

    InvalidRequestArgumentError增加error_code (#885)
    * 文档优化
    
    * InvalidRequestArgumentError增加error_code
    
    * InvalidRequestArgumentError增加error_code
    
    ---------
    
    Co-authored-by: yepeiwen01 <yepeiwen01@bidu.com>

--- python/core/_exception.py
+++ python/core/_exception.py
@@ -148,7 +148,16 @@ class AssistantServerException(BaseRPCException):
 class InvalidRequestArgumentError(BaseRPCException):
     r"""InvalidRequestArgumentError invalid request param
     """
-    pass
+    description: str = "Invalid Request Params Error"
+    code: int = 400
+
+    def __init__(self, message="", request_id= "", code=""):
+        self.description = "request_id={}, code={}, message={}".format(
+            request_id, code, message)
+        self.code = code if code else self.code
+
+    def __str__(self):
+        return self.description
 
 
 class RiskInputException(BaseRPCException):
